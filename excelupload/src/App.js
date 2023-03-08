522e08ad9883da362a5f7efec37c7478efc05b4c
mingg123 <spgb123spgb@gmail.co.kr>
2023-03-08 11:26:12 +0900

    feat: parseData

--- excelupload/src/App.js
+++ excelupload/src/App.js
@@ -4,6 +4,15 @@ import "./App.css";
 
 function App() {
   const [excelData, setExcelData] = useState([]);
+  const [parseData, setParseData] = useState([]);
+
+  function changeLabel(data) {
+    return data === "요율" ? "PERCENTAGE" : "VALUE";
+  }
+
+  function changeNullLabel(data) {
+    return data === "" ? null : data;
+  }
 
   const excelExport = (event) => {
     const input = event.target;
@@ -14,7 +23,28 @@ function App() {
       const wb = XLSX.read(fileData, { type: "binary" });
       wb.SheetNames.forEach((sheetName) => {
         const rowObj = XLSX.utils.sheet_to_json(wb.Sheets[sheetName]);
-        console.log("123456", rowObj);
+
+        let parseList = [];
+        rowObj.map((row) => {
+          const discount =
+            row.할인금액 === ""
+              ? null
+              : {
+                  name: row.할인명,
+                  value: row.할인금액,
+                  type: changeLabel(row.할인유형),
+                };
+          const newRow = {
+            color: changeNullLabel(row.포스키색상),
+            label: changeNullLabel(row.포스키이름),
+            productInfoId: changeNullLabel(row.상품번호),
+            discountInfo: discount,
+          };
+
+          parseList.push(newRow);
+        });
+
+        setParseData(parseList);
         setExcelData(rowObj);
       });
     };
@@ -25,6 +55,7 @@ function App() {
     <div className="App">
       <input type="file" onChange={excelExport} />
       {excelData.length > 0 && excelData[0].포스키이름}
+      {parseData.toString()}
     </div>
   );
 }
